0a7bdb894d9b22a6b18b6ca8b41eb1bc0f7e2a8c
mathieu4635 <127602837+mathieu4635@users.noreply.github.com>
2023-05-23 10:28:00 +0200

    Add files via upload

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,13 +1,42 @@
+/*************************************************************************
+                           Sensor  -  description
+                             -------------------
+    début                : $23/05/2023$
+    copyright            : (C) $2023$ par $AirWatcher$
+    e-mail               : $mathieu.habran@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Sensor> (fichier Sensor.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
 #include <string>
 using namespace std;
 
-#include "Sensor.h"
+//------------------------------------------------------ Include personnel
 
-Sensor(string id, double latitude, double longitude){
+#include "Sensor.h"
+#include "Position.h"
+
+//----------------------------------------------------- Méthodes publiques
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+
+Sensor::Sensor(string id, double latitude, double longitude){
+
 	id = identifiant;
 	position = new Position(latitude, longitude);
-}
+} //----- Fin de Sensor
 
-~Sensor(){
+~Sensor::Sensor(){
 	delete Position;
-}
+} //----- Fin de Sensor
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
--- src/Sensor.h
+++ src/Sensor.h
@@ -1,14 +1,54 @@
+/*************************************************************************
+                           Sensor  -  description
+                             -------------------
+    début                : $23/05/2023$
+    copyright            : (C) $2023$ par $AirWatcher$
+    e-mail               : $mathieu.habran@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Interface de la classe <Sensor> (fichier Sensor.h) -------------
+#if ! defined ( SENSOR_H )
+#define SENSOR_H
+
+//--------------------------------------------------- Interfaces utilisées
 #include <string>
+#include <vector>
+
+#include "Measurement.h"
+#include "Position.h"
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Sensor>
+// desc
+//------------------------------------------------------------------------
+
 using namespace std;
 
-public class Sensor{
+class Sensor{
+	
+//----------------------------------------------------------------- PUBLIC
 
+public:
+
+//----------------------------------------------------- Méthodes publiques
+
+	
+	Sensor(string id, double latitude, double longitude);
+	// Mode d'emploi :
+
+	~Sensor();
+	// Mode d'emploi :
+	
 	string id; 
 	User* user;
 	Position position;	
-	Vector <Measurement*> measurements;
-	
-	Sensor(string id, double latitude, double longitude);
-	~Sensor();
+	vector <Measurement*> measurements;
 
 }
+
+//------------------------------- Autres définitions dépendantes de <Sensor>
+
+#endif // SENSOR_H
