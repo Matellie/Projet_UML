289ce86f0ac93779d3554a20dc0b1282ea8fb13e
Matellie <jeanrazier@gmail.com>
2023-05-23 09:48:28 +0200

    creation Data

--- src/Data.h
+++ src/Data.h
@@ -1,11 +1,80 @@
-#ifndef DATA_H
+/*************************************************************************
+                           Data  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Data> (fichier Data.h) ----------------
+#if ! defined ( DATA_H )
 #define DATA_H
+
+//--------------------------------------------------- Interfaces utilisées
+using namespace std;
+#include <stdlib.h>
+//#include "User.h"
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Data>
+//
+//
+//------------------------------------------------------------------------
 
 class Data
 {
+//----------------------------------------------------------------- PUBLIC
+
 public:
-    Data();
-    ~Data();
+//----------------------------------------------------- Attributs publics
+//unordered_map<string id, User* user> users;
+//unordered_map<string id, Sensor* sensor> sensors;
+//vector<Measurement*> measurements;
+
+//----------------------------------------------------- Méthodes publiques
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Data & operator = ( const Data & unData );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Data ( const Data & unData );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Data ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    ~Data ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
 };
 
-#endif
+//-------------------------------- Autres définitions dépendantes de <Data>
+
+#endif // DATA_H
+
